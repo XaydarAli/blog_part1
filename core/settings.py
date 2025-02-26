50a127824dd502b242e2c7910a8fc9c8a0e65e05
XaydarAli <khaydarovalijon1308@gmail.com>
2025-02-27 00:24:52 +0500

    adding the next part

--- core/settings.py
+++ core/settings.py
@@ -126,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='khaydarovalijon1308@gmail.com'
+EMAIL_HOST_PASSWORD="+998907155152"
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+
+
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
